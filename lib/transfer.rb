83eecb4160f42cbae2a89b54822507af7f150806
Steve Cordova <cordovas@bc.edu>
2020-08-11 02:53:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @status == "pending"
+    if @status == "pending" && self.valid?
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       @status = "complete"
