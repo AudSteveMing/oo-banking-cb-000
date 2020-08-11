202b1773c38012f0afa0d851f21c2bdbc31779a4
Steve Cordova <cordovas@bc.edu>
2020-08-11 02:57:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,6 @@ class Transfer
       @status = "complete"
     else
       @status = "rejected"
-
       "Transaction rejected. Please check your account balance."
       # if @status == "complete"
       # @sender.balance
@@ -53,6 +52,10 @@ class Transfer
   end
 
   def reverse_transfer
+    if @status == "complete"
+      sender.balance = sender.balance + amount
+      receiver.balance = receiver.balance - amount
+    else
   end
   # your code here
 end
